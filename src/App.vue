a1a60e073094c9e95c3f33afbbf64541838703d4
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2025-04-04 18:04:53 -0300

    feat: initial setup vue.js + conditional ifs in vue.js

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader, // Registro
+  },
+};
+</script>
+
+<template>
+  <TheHeader />
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: `Seja bem vindo:`,
+      showHeader: true,
+      acessLevel: "Marketing",
+    };
+  },
+};
+</script>
+
+<template>
+  <header v-show="showHeader" class="header">
+    <h1 v-if="acessLevel === 'Admin'">
+      {{ msg }} <span>{{ acessLevel }}</span>
+    </h1>
+    <h1 v-if="acessLevel === 'Marketing'">
+      {{ msg }} <span>{{ acessLevel }}</span>
+    </h1>
+    <h1 v-if="acessLevel === 'User'">
+      {{ msg }} <span>{{ acessLevel }}</span>
+    </h1>
+
+    <nav>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  width: 90ch;
+  margin: 0 auto;
+}
+.header h1 {
+  color: white;
+}
+
+.header nav ul {
+  display: flex;
+  gap: 1.5rem;
+}
+
+.header nav ul li {
+}
+</style>
